fc6c14884dcb4a0cfb1dfc0d349e484e66600f02
夕阳醉了 <400822@163.com>
2018-03-31 12:52:28 +0800

    搜索页面search-box组件开发

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    搜索页面
+    <search-box></search-box>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import SearchBox from 'base/search-box/search-box'
+  export default {
+    components: {
+      SearchBox
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
