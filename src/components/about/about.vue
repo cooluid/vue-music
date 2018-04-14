2b50c79261d12942907d708a88119cf190bfb23f
cooluid <173000@qq.com>
2018-04-15 03:06:46 +0800

    关于

--- src/components/about/about.vue
+++ src/components/about/about.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="about">
-        <p class="text">强大的音乐播放器</p>
-        <div class="href"><a href="http://www.xxxx.cool" target="_blank">www.xxxx.cool</a></div>
+        <div class="icon"></div>
+        <h1 class="text">动听音乐播放器</h1>
+        <p class="desc">MVVM框架：Vue.js （版本2.x）<br>
+            状态管理：Vuex  <br>
+            前端路由：Vue Router  <br>
+            服务端通讯：axios、jsonp <br>
+            移动端滚动库：better-scroll <br>
+            构建工具：webpack 2.0 <br>
+            数据：QQ音乐 <br>
+            源码：es6
+        </p>
+        <p class="blog">
+            blog: xxxx.cool
+        </p>
     </div>
 </template>
 
@@ -13,11 +25,26 @@
     @import "~common/stylus/variable"
     @import "~common/stylus/mixin"
     .about
-        margin: 50px
         color: $color-text-l
+        text-align: center
+        margin-top: 80px
+        .icon
+            display: inline-block
+            width: 30px
+            height: 30px
+            bg-image('logo')
+            background-size: 35px 35px
         .text
-            text-align: center
-        .href
-            text-align: center
-            margin-top: 50px
+            display: inline-block
+            line-height: 36px
+            font-size: $font-size-large
+            vertical-align: top
+        .desc
+            text-align: left
+            margin: 30px 0 0 50px
+            line-height: 30px
+        .blog
+            text-align: right
+            margin: 100px 10px 0 0
+            color: #222e2f
 </style>
