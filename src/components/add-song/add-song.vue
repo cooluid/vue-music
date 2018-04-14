1b41dd55c2a596aefcd40e59a5cc34472566a08e
夕阳醉了 <400822@163.com>
2018-04-14 14:47:31 +0800

    歌曲列表组件top-list组件实现

--- src/components/add-song/add-song.vue
+++ src/components/add-song/add-song.vue
@@ -1,36 +1,43 @@
 <template>
-    <transition name="slide">
-        <div class="add-song" v-show="showFlag" @click.stop>
-            <div class="header">
-                <h1 class="title">添加歌曲到列表</h1>
-                <div class="close" @click="hide">
-                    <i class="icon-close"></i>
-                </div>
-            </div>
-            <div class="search-box-wrapper">
-                <search-box ref="searchBox" @query="queryChange" placeholder="搜索歌曲"></search-box>
-            </div>
-            <div class="shortcut" v-show="!query">
-                <switches :switches="switches" :currentIndex="currentIndex" @switch="switchItem"></switches>
-                <div class="list-wrapper">
-                    <scroll ref="songList" class="list-scroll" v-if="currentIndex === 0" :data="playHistory">
-                        <div class="list-inner">
-                            <song-list :songs="playHistory" @select="selectSong"></song-list>
-                        </div>
-                    </scroll>
-                    <scroll ref="searchList" class="list-scroll" v-if="currentIndex === 1" :data="searchHistory">
-                        <div class="list-inner">
-                            <search-list @delete="deleteSearchHistory" @select="addQuery"
-                                         :searches="searchHistory"></search-list>
-                        </div>
-                    </scroll>
-                </div>
-            </div>
-            <div class="search-result" v-show="query">
-                <suggest @select="selectSuggest" :query="query" :showSinger="showSinger"></suggest>
-            </div>
+  <transition name="slide">
+    <div class="add-song" v-show="showFlag" @click.stop>
+      <div class="header">
+        <h1 class="title">添加歌曲到列表</h1>
+        <div class="close" @click="hide">
+          <i class="icon-close"></i>
         </div>
-    </transition>
+      </div>
+      <div class="search-box-wrapper">
+        <search-box ref="searchBox" @query="queryChange" placeholder="搜索歌曲"></search-box>
+      </div>
+      <div class="shortcut" v-show="!query">
+        <switches :switches="switches" :currentIndex="currentIndex" @switch="switchItem"></switches>
+        <div class="list-wrapper">
+          <scroll ref="songList" class="list-scroll" v-if="currentIndex === 0" :data="playHistory">
+            <div class="list-inner">
+              <song-list :songs="playHistory" @select="selectSong"></song-list>
+            </div>
+          </scroll>
+          <scroll ref="searchList" class="list-scroll" v-if="currentIndex === 1" :data="searchHistory">
+            <div class="list-inner">
+              <search-list @delete="deleteSearchHistory" @select="addQuery"
+                           :searches="searchHistory"></search-list>
+            </div>
+          </scroll>
+        </div>
+      </div>
+      <div class="search-result" v-show="query">
+        <suggest @select="selectSuggest" :query="query" :showSinger="showSinger"></suggest>
+      </div>
+      <top-tip ref="topTip">
+        <div class="tip-title">
+          <i class="icon-ok">
+            <span class="text">1首歌曲已经添加到播放队列</span>
+          </i>
+        </div>
+      </top-tip>
+    </div>
+  </transition>
 </template>
 
 <script type="text/ecmascript-6">
@@ -43,6 +50,7 @@
   import SongList from 'base/song-list/song-list'
   import Song from 'common/js/song'
   import SearchList from 'base/search-list/search-list'
+  import TopTip from 'base/top-tip/top-tip'
 
   export default {
     mixins: [searchMixin],
@@ -68,13 +76,18 @@
       selectSong (song, index) {
         if (index !== 0) {
           this.instertSong(new Song(song))
+          this.showTip()
         }
+      },
+      showTip() {
+        this.$refs.topTip.show()
       },
       switchItem (index) {
         this.currentIndex = index
       },
       selectSuggest () {
         this.saveSearch()
+        this.showTip()
       },
       refreshScroll () {
         setTimeout(() => {
@@ -107,69 +120,70 @@
       Switches,
       Scroll,
       SongList,
-      SearchList
+      SearchList,
+      TopTip
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-    @import "~common/stylus/variable"
-    @import "~common/stylus/mixin"
-    .add-song
-        position: fixed
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+  .add-song
+    position: fixed
+    top: 0
+    bottom: 0
+    width: 100%
+    z-index: 200
+    background: $color-background
+    &.slide-enter-active, &.slide-leave-active
+      transition: all 0.3s
+    &.slide-enter, &.slide-leave-to
+      transform: translate3d(100%, 0, 0)
+    .header
+      position: relative
+      height: 44px
+      text-align: center
+      .title
+        line-height: 44px
+        font-size: $font-size-large
+        color: $color-text
+      .close
+        position: absolute
         top: 0
+        right: 8px
+        .icon-close
+          display: block
+          padding: 12px
+          font-size: 20px
+          color: $color-theme
+    .search-box-wrapper
+      margin: 20px
+    .shortcut
+      .list-wrapper
+        position: absolute
+        top: 165px
         bottom: 0
         width: 100%
-        z-index: 200
-        background: $color-background
-        &.slide-enter-active, &.slide-leave-active
-            transition: all 0.3s
-        &.slide-enter, &.slide-leave-to
-            transform: translate3d(100%, 0, 0)
-        .header
-            position: relative
-            height: 44px
-            text-align: center
-            .title
-                line-height: 44px
-                font-size: $font-size-large
-                color: $color-text
-            .close
-                position: absolute
-                top: 0
-                right: 8px
-                .icon-close
-                    display: block
-                    padding: 12px
-                    font-size: 20px
-                    color: $color-theme
-        .search-box-wrapper
-            margin: 20px
-        .shortcut
-            .list-wrapper
-                position: absolute
-                top: 165px
-                bottom: 0
-                width: 100%
-                .list-scroll
-                    height: 100%
-                    overflow: hidden
-                    .list-inner
-                        padding: 20px 30px
-        .search-result
-            position: fixed
-            top: 124px
-            bottom: 0
-            width: 100%
-        .tip-title
-            text-align: center
-            padding: 18px 0
-            font-size: 0
-            .icon-ok
-                font-size: $font-size-medium
-                color: $color-theme
-                margin-right: 4px
-            .text
-                font-size: $font-size-medium
-                color: $color-text
+        .list-scroll
+          height: 100%
+          overflow: hidden
+          .list-inner
+            padding: 20px 30px
+    .search-result
+      position: fixed
+      top: 124px
+      bottom: 0
+      width: 100%
+    .tip-title
+      text-align: center
+      padding: 18px 0
+      font-size: 0
+      .icon-ok
+        font-size: $font-size-medium
+        color: $color-theme
+        margin-right: 4px
+      .text
+        font-size: $font-size-medium
+        color: $color-text
 </style>
