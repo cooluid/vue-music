884b24e7c2946b58c578362cbbc501b6dd3e65a9
cooluid <173000@qq.com>
2018-04-15 01:27:43 +0800

    移动端调试工具和抓包工具介绍

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-import 'common/stylus/index.styl'
 import VueLazyLoad from 'vue-lazyload'
 import store from './store'
+
+import 'common/stylus/index.styl'
+
+import VConsole from 'vconsole'
+/* eslint-disable no-unused-vars */
+var vConsole = new VConsole()
+console.log('test')
 
 fastclick.attach(document.body)
 Vue.use(VueLazyLoad, {
