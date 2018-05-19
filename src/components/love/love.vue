d0bfe575c7abe4fdac8659828949c2796944a016
cooluid <173000@qq.com>
2018-05-20 06:43:47 +0800

    .

--- src/components/love/love.vue
+++ src/components/love/love.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="love">
+    <div class="love" @click="stageTouch">
         <div class="background">
             <img width="100%" height="100%" src="./lovebg.jpeg">
         </div>
+        <div class="tips" v-show="!isinit">点击播放</div>
     </div>
 </template>
 
@@ -11,22 +12,28 @@
   import Song from 'common/js/song'
 
   export default {
-    created () {
-      this.$nextTick(() => {
-        let song = new Song({
-          'id': 107716352,
-          'mid': '000gRkVb3DOM3e',
-          'singer': '小贱（谭冰尧）/可歆',
-          'name': '小温馨',
-          'album': '小温馨',
-          'duration': 213,
-          'image': 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004bPW7U2NwQDc.jpg?max_age=2592000',
-          'url': 'http://dl.stream.qqmusic.qq.com/http://dl.stream.qqmusic.qq.com/C400000gRkVb3DOM3e.m4a?guid=2140998078&vkey=328B78E7DA9775FFBBD2D36FC4118CC7A5D8C3AC993909399F722B2AEFAFBA80BCCD6FC3C7B7D4B9B02B24934B12936354A07A82551298B7&uin=0&fromtag=38'
-        })
-        this.insertSong(song)
-      })
+    data () {
+      return {
+        isinit: false
+      }
     },
     methods: {
+      stageTouch () {
+        this.isinit = true
+        this.$nextTick(() => {
+          let song = new Song({
+            'id': 107716352,
+            'mid': '000gRkVb3DOM3e',
+            'singer': '小贱（谭冰尧）/可歆',
+            'name': '小温馨',
+            'album': '小温馨',
+            'duration': 213,
+            'image': 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004bPW7U2NwQDc.jpg?max_age=2592000',
+            'url': 'http://dl.stream.qqmusic.qq.com/http://dl.stream.qqmusic.qq.com/C400000gRkVb3DOM3e.m4a?guid=2140998078&vkey=328B78E7DA9775FFBBD2D36FC4118CC7A5D8C3AC993909399F722B2AEFAFBA80BCCD6FC3C7B7D4B9B02B24934B12936354A07A82551298B7&uin=0&fromtag=38'
+          })
+          this.insertSong(song)
+        })
+      },
       ...mapActions([
         'insertSong'
       ])
@@ -35,16 +42,18 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+    @import "~common/stylus/variable"
+    @import "~common/stylus/mixin"
     .love
         width: 100%
-        text-align: center
         .background
-            position: absolute
-            left: 0
-            top: 100px
-            width: 100%
-            /*height: 100%*/
+            /*position: absolute*/
+            /*left: 0*/
+            margin-top: 12px
+            /*width: 100%*/
             z-index: -1
-            .text
-                margin-top: 10px
+        .tips
+            bottom: 50px
+            color: $color-theme
+            text-align: center
 </style>
--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="m-header">
-        <div class="icon"></div>
+        <!--<div class="icon"></div>-->
         <h1 class="text">happy</h1>
         <router-link tag="div" class="mine" to="/user">
             <i class="icon-mine"></i>
